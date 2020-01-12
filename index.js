9e53dd7ecec95a2ebcb681eafbe529ff9c48a5d6
Dayana Matsakova <Dayana.Matsakova89@gmail.com>
2020-01-12 14:20:52 -0500

    Eslintrc added

--- index.js
+++ index.js
@@ -1,65 +1,66 @@
-function sum (a,b){
-    return a + b;
-}
-function mult(a, b){
-    return a * b;
+function sum(a, b) {
+  return a + b;
 }
 
-function isAdult(number){
-    if (number < 21 && number >= 0) return false;
-    else if (number >= 21) return true;
+function mult(a, b) {
+  return a * b;
 }
 
-function isPalindrome(str){
-    let s = '';
-    if (typeof(str) === 'string') {
-        for (let i = str.length - 1; i >= 0; i--) {
-            s += str[i];
-        }
-        return s.toLowerCase() === str.toLowerCase();
-    } else return 'this is not a string!';
+function isAdult(number) {
+  if (number < 21 && number >= 0) return false;
+  else if (number >= 21) return true;
 }
 
-function noSpace(x){
-    return x.split(' ').join('');
+function isPalindrome(str) {
+  let s = '';
+  if (typeof (str) === 'string') {
+    for (let i = str.length - 1; i >= 0; i--) {
+      s += str[i];
+    }
+    return s.toLowerCase() === str.toLowerCase();
+  } else return 'this is not a string!';
+}
+
+function noSpace(x) {
+  return x.split(' ').join('');
 }
 
 function maxValue(arr) {
-    if (Array.isArray(arr)) return Math.max(...arr);
-    else return undefined;
+  if (Array.isArray(arr)) return Math.max(...arr);
+  else return undefined;
 }
 
-function monkeyCount(n){
-    const array = [];
-    if (n < 0) return undefined;
-    else {
-        for (let i = 1; i <= n; i++) {
-            array.push(i);
-        }
+function monkeyCount(n) {
+  const array = [];
+  if (n < 0) return undefined;
+  else {
+    for (let i = 1; i <= n; i++) {
+      array.push(i);
     }
-    return array;
+  }
+  return array;
 }
 
-function divide(num1, num2){
-    if (num2 === 0) return null;
-    else return num1 / num2;
+function divide(num1, num2) {
+  if (num2 === 0) return null;
+  else return num1 / num2;
 }
 
-function outed(meet, boss){
-    let sum = 0;
-    let count = 0;
-    for(let key in meet) {
-        if (key === boss) sum = sum + meet[key] + meet[boss];
-        else sum = sum + meet[key];
-        count += 1;
-    }
-    return (sum / count <= 5) ? 'Get Out Now!' : 'Nice Work Champ!';
+function outed(meet, boss) {
+  let sum = 0;
+  let count = 0;
+  for (let key in meet) {
+    if (key === boss) sum = sum + meet[key] + meet[boss];
+    else sum = sum + meet[key];
+    count += 1;
+  }
+  return (sum / count <= 5) ? 'Get Out Now!' : 'Nice Work Champ!';
 }
 
 
-function addingProperty(obj){
-    obj.name = 'Sara';
-    return obj;
+function addingProperty(obj) {
+  obj.name = 'Sara';
+  return obj;
 }
 
 module.exports = {sum, mult, isAdult, isPalindrome, noSpace, maxValue, monkeyCount, divide, outed, addingProperty};
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,171 +1,170 @@
-const assert = require('assert');
 const {expect} = require('chai');
 
 const {sum, mult, isAdult, isPalindrome, noSpace, maxValue, monkeyCount, divide, outed, addingProperty} = require ('../index.js');
 
 describe('function sum',() => {
-    it('should function sum equal 6', () => {
-        expect(sum(2,4)).eq(6);
-    });
+  it('should function sum equal 6', () => {
+    expect(sum(2,4)).eq(6);
+  });
 
-    it('should function sum not equal 0', () => {
-        expect(sum(2, 4)).to.not.equal(0);
-    });
+  it('should function sum not equal 0', () => {
+    expect(sum(2, 4)).to.not.equal(0);
+  });
 });
 
 it('should function mult equal 8', () => {
-    expect(mult(2,4)).eq(8);
+  expect(mult(2,4)).eq(8);
 });
 
 describe('function isAdult', () => {
-    it('should function isAdult(16) return false', () => {
-        expect(isAdult(16)).false;
-    });
+  it('should function isAdult(16) return false', () => {
+    expect(isAdult(16)).false;
+  });
 
-    it('should function isAdult(33) return true', () => {
-        expect(isAdult(33)).true;
-    });
+  it('should function isAdult(33) return true', () => {
+    expect(isAdult(33)).true;
+  });
 
-    it('should function isAdult(-5) not to return true', () => {
-        expect(isAdult(-5)).not.true;
-    });
+  it('should function isAdult(-5) not to return true', () => {
+    expect(isAdult(-5)).not.true;
+  });
 
-    it('should function isAdult(-88) return undefined', () => {
-        expect(isAdult(-88)).undefined;
-    });
+  it('should function isAdult(-88) return undefined', () => {
+    expect(isAdult(-88)).undefined;
+  });
 
-    it('should function isAdult("abcdef") return undefined', () => {
-        expect(isAdult('abcdef')).undefined;
-    });
+  it('should function isAdult("abcdef") return undefined', () => {
+    expect(isAdult('abcdef')).undefined;
+  });
 
-    it('should function isAdult() return undefined', () => {
-       expect(isAdult()).undefined;
-    });
+  it('should function isAdult() return undefined', () => {
+    expect(isAdult()).undefined;
+  });
 });
 
 describe('function isPalindrome', () => {
-    it('should function isPalindrome("ogo") return true', () => {
-        expect(isPalindrome('ogo')).true;
-    });
+  it('should function isPalindrome("ogo") return true', () => {
+    expect(isPalindrome('ogo')).true;
+  });
 
-    it('should function isPalindrome("abc") return false', () => {
-        expect(isPalindrome('abc')).false;
-    });
+  it('should function isPalindrome("abc") return false', () => {
+    expect(isPalindrome('abc')).false;
+  });
 
-    it('should function isPalindrome("AbBa") return true', () => {
-        expect(isPalindrome('AbBa')).true;
-    });
+  it('should function isPalindrome("AbBa") return true', () => {
+    expect(isPalindrome('AbBa')).true;
+  });
 
-    it('should function isPalindrome(ogo AbBa") return false', () => {
-        expect(isPalindrome('ogo AbBa')).false;
-    });
+  it('should function isPalindrome(ogo AbBa") return false', () => {
+    expect(isPalindrome('ogo AbBa')).false;
+  });
 
-    it('should function isPalindrome("") return true', () => {
-        expect(isPalindrome('')).true;
-    });
+  it('should function isPalindrome("") return true', () => {
+    expect(isPalindrome('')).true;
+  });
 
-    it('should function isPalindrome(111) return "this is not a string!" ', () => {
-        expect(isPalindrome(111)).eq('this is not a string!');
-    });
+  it('should function isPalindrome(111) return "this is not a string!" ', () => {
+    expect(isPalindrome(111)).eq('this is not a string!');
+  });
 
-    it('should function isPalindrome([5,6,7]) return "this is not a string!" ', () => {
-        expect(isPalindrome([5,6,7])).eq('this is not a string!');
-    });
+  it('should function isPalindrome([5,6,7]) return "this is not a string!" ', () => {
+    expect(isPalindrome([5,6,7])).eq('this is not a string!');
+  });
 });
 
 describe ('function noSpace', () => {
-    it ('should function noSpace("hello world how are you") return "helloworldhowareyou" ', () => {
-        expect(noSpace('hello world how are you')).eq('helloworldhowareyou');
-    });
+  it ('should function noSpace("hello world how are you") return "helloworldhowareyou" ', () => {
+    expect(noSpace('hello world how are you')).eq('helloworldhowareyou');
+  });
 
-    it('should function noSpace("abcdefg") return "abcdefg" ', () => {
-        expect(noSpace('abcdefg')).eq('abcdefg');
-    });
+  it('should function noSpace("abcdefg") return "abcdefg" ', () => {
+    expect(noSpace('abcdefg')).eq('abcdefg');
+  });
 
-    it('should function noSpace("   abc def  ") return "abcdef" ', () => {
-        expect(noSpace('   abc def  ')).eq('abcdef');
-    });
+  it('should function noSpace("   abc def  ") return "abcdef" ', () => {
+    expect(noSpace('   abc def  ')).eq('abcdef');
+  });
 
-    it('should function noSpace("  3 qwerty\n\rabcd " ") return "3qwerty\n\rabcd" ', () => {
-      expect(noSpace('  3 qwerty\n\rabcd ')).eq('3qwerty\n\rabcd');
-    });
+  it('should function noSpace("  3 qwerty\n\rabcd " ") return "3qwerty\n\rabcd" ', () => {
+    expect(noSpace('  3 qwerty\n\rabcd ')).eq('3qwerty\n\rabcd');
+  });
 });
 
 describe ('function maxValue', () => {
-    it ('should function maxValue([73,2,50,4,99,0]) return 99', () => {
-        expect(maxValue([73,2,50,4,99,0])).eq(99);
-    });
+  it ('should function maxValue([73,2,50,4,99,0]) return 99', () => {
+    expect(maxValue([73,2,50,4,99,0])).eq(99);
+  });
 
-    it ('should function maxValue([9,7,16,88,-6,3]) not to return 7', () => {
-        expect(maxValue([9,7,16,88,-6,3])).not.eq(7);
-    });
+  it ('should function maxValue([9,7,16,88,-6,3]) not to return 7', () => {
+    expect(maxValue([9,7,16,88,-6,3])).not.eq(7);
+  });
 
-    it('should function maxValue("123456") return undefined', () => {
-        expect(maxValue("123456")).undefined;
-    });
+  it('should function maxValue("123456") return undefined', () => {
+    expect(maxValue('123456')).undefined;
+  });
 });
 
 
 describe ('function monkeyCount', () => {
-    it ('should function monkeyCount(3) return [1,2,3]', () => {
-        expect(monkeyCount(3)).deep.eq([1,2,3]);
-    });
+  it ('should function monkeyCount(3) return [1,2,3]', () => {
+    expect(monkeyCount(3)).deep.eq([1,2,3]);
+  });
 
-    it ('should function monkeyCount(0) return []', () => {
-        expect(monkeyCount(0)).deep.eq([]);
-    });
+  it ('should function monkeyCount(0) return []', () => {
+    expect(monkeyCount(0)).deep.eq([]);
+  });
 
-    it ('should function monkeyCount(-7) return undefined', () => {
-        expect(monkeyCount(-7)).undefined;
-    });
+  it ('should function monkeyCount(-7) return undefined', () => {
+    expect(monkeyCount(-7)).undefined;
+  });
 });
 
 
 describe ('function divide', () => {
-    it ('should function divide(2,0) return null', () => {
-        expect(divide(2,0)).null;
-    });
+  it ('should function divide(2,0) return null', () => {
+    expect(divide(2,0)).null;
+  });
 
-    it ('should function divide(15,5) return 3', () => {
-        expect(divide(15,5)).eq(3);
-    });
+  it ('should function divide(15,5) return 3', () => {
+    expect(divide(15,5)).eq(3);
+  });
 
-    it ('should function divide(0,-7) return 0', () => {
-        expect(divide(0,-7)).eq(0);
-    });
+  it ('should function divide(0,-7) return 0', () => {
+    expect(divide(0,-7)).eq(0);
+  });
 
-    it('should function divide("a",2) return NaN', () => {
-       expect(divide("a", 2)).NaN;
-    });
+  it('should function divide("a",2) return NaN', () => {
+    expect(divide('a', 2)).NaN;
+  });
 });
 
 describe ('function outed', () => {
-    it ('should function outed return "Get Out Now!" ', () => {
-       expect(outed({'tim':0, 'jim':2, 'randy':0, 'sandy':7, 'andy':0, 'katie':5, 'laura':1, 'saajid':2, 'alex':3, 'john':2, 'mr':0}, 'laura')).eq('Get Out Now!');
-    });
+  it ('should function outed return "Get Out Now!" ', () => {
+    expect(outed({'tim':0, 'jim':2, 'randy':0, 'sandy':7, 'andy':0, 'katie':5, 'laura':1, 'saajid':2, 'alex':3, 'john':2, 'mr':0}, 'laura')).eq('Get Out Now!');
+  });
 
-    it ('should function outed return "Nice Work Champ!" ', () => {
-        expect(outed({'tim':1, 'jim':3, 'randy':9, 'sandy':6, 'andy':7, 'katie':6, 'laura':9, 'saajid':9, 'alex':9, 'john':9, 'mr':8}, 'katie')).eq('Nice Work Champ!');
-    });
+  it ('should function outed return "Nice Work Champ!" ', () => {
+    expect(outed({'tim':1, 'jim':3, 'randy':9, 'sandy':6, 'andy':7, 'katie':6, 'laura':9, 'saajid':9, 'alex':9, 'john':9, 'mr':8}, 'katie')).eq('Nice Work Champ!');
+  });
 });
 
 
 describe ('function addingProperty', () => {
-    it ('should function addingProperty return {"age": 21, "occupancy": "actor", "name": "Sara"}', () => {
-        expect(addingProperty({'age': 21, 'occupancy': 'actor'})).deep.eq({'age': 21, 'occupancy': 'actor', 'name': 'Sara'});
-    });
+  it ('should function addingProperty return {"age": 21, "occupancy": "actor", "name": "Sara"}', () => {
+    expect(addingProperty({'age': 21, 'occupancy': 'actor'})).deep.eq({'age': 21, 'occupancy': 'actor', 'name': 'Sara'});
+  });
 
-    it ('should function addingProperty return {"name": "Sara", "age": 35}', () => {
-        expect(addingProperty({'name': 'Michelle', 'age': 35})).deep.eq({'name': 'Sara', 'age': 35});
-    });
+  it ('should function addingProperty return {"name": "Sara", "age": 35}', () => {
+    expect(addingProperty({'name': 'Michelle', 'age': 35})).deep.eq({'name': 'Sara', 'age': 35});
+  });
 
-    it('should function addingproperty([1,2,3]) return [1,2,3]', () => {
-        expect(addingProperty([1,2,3])).deep.eq([1,2,3]);
-    });
+  it('should function addingproperty([1,2,3]) return [1,2,3]', () => {
+    expect(addingProperty([1,2,3])).deep.eq([1,2,3]);
+  });
 
-    it('should function addingProperty("abcdef") return "abcdef" ', () => {
-        expect(addingProperty("abcdef")).eq("abcdef");
-    });
+  it('should function addingProperty("abcdef") return "abcdef" ', () => {
+    expect(addingProperty('abcdef')).eq('abcdef');
+  });
 });
 
 
